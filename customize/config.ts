d4ddee008d8a9678df3dec3121f3c05fbfd58e09
Shuukaa <69614231+Shuukaa@users.noreply.github.com>
2026-09-03 18:45:01 -0500

    Update config.ts

--- customize/config.ts
+++ customize/config.ts
@@ -9,7 +9,13 @@ export const config = {
   // Optional voting deadline (ISO 8601). Written into the generated sorter-index.json.
   deadline: new Date("2026-09-05T04:00:00.000Z"),
   // Uncomment the following lines to enable Google Sheets integration. Make sure to fill in the correct values.
- googleSheets: {
-    scoreColumnHeader: "Score (optional)",
- },
+   googleSheets: {
+     clientId: "859512107931-vghv0ldr90a6ilc14ug3g3rk3b2eg43b.apps.googleusercontent.com",
+     appId: "859512107931",
+     rankColumnHeader: "Rank",
+  //   This setting will only work if `scoreColumnHeader` is not set, allowing users to specify their own score column header in the sheet.
+  //   If `scoreColumnHeader` is set, that value will be used as the score column header and users won't be able to change it.
+  //   allowCustomScoreColumn: true,
+     scoreColumnHeader: "Score (optional)",
+  // },
 } satisfies AppConfig;
